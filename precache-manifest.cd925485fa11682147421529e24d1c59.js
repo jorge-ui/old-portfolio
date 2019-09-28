5ee4589d0ac6c977afc3b6232dcde50675b01b0b
Jorge Rivera <jrdeveloper71@gmail.com>
2019-09-27 22:08:52 -0400

    Updates

---
+++ precache-manifest.cd925485fa11682147421529e24d1c59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be6b43700c3d7f8e517c451b9d51bf8",
+    "url": "/old-portfolio/index.html"
+  },
+  {
+    "revision": "f0d4601fad737c88dd4d",
+    "url": "/old-portfolio/static/css/main.7526c14c.chunk.css"
+  },
+  {
+    "revision": "81ff05eb6ed5725b9ce4",
+    "url": "/old-portfolio/static/js/2.6fdac53c.chunk.js"
+  },
+  {
+    "revision": "f0d4601fad737c88dd4d",
+    "url": "/old-portfolio/static/js/main.35d10fc1.chunk.js"
+  },
+  {
+    "revision": "9c5d8c3adb2dfad62ffc",
+    "url": "/old-portfolio/static/js/runtime~main.cdb992f9.js"
+  },
+  {
+    "revision": "1c58619c4180f1345cc2299d16d683d5",
+    "url": "/old-portfolio/static/media/devConnector.1c58619c.png"
+  },
+  {
+    "revision": "cc90cc907742724d6cb3d1bc685c2dd0",
+    "url": "/old-portfolio/static/media/me-new.min.cc90cc90.jpg"
+  },
+  {
+    "revision": "2cdda676e0632246ddf35abef9f47c93",
+    "url": "/old-portfolio/static/media/spaceVisualizer.2cdda676.png"
+  }
+]);
